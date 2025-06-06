56cbc28d9147ca8170f982bd0b589d09ca01f395
Harshapendli <harshareddypendli@gmail.com>
2025-06-06 17:37:49 +0530

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -15,7 +15,7 @@ with st.sidebar:
 chat = None
 if api_key:
     try:
-        model = genai.GenerativeModel("Gemini 2.5 Flash Preview 05-20")
+        model = genai.GenerativeModel("gemini-2.5-flash-preview-05-20")
         chat = model.start_chat(history=[])
     except Exception as e:
         st.error(f"Error initializing Gemini model: {e}")
